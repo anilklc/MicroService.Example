291753389098c28b79902f7d7ce7a9b86d5e496a
anilklc <hage4950@gmail.com>
2024-07-28 17:01:27 +0300

    API Gateway Added

---
+++ APIGateway/Program.cs
@@ -0,0 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot().AddConsul();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseOcelot().Wait();
+
+app.Run();
